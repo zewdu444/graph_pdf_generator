0f9b43287647ccf496e518a76133515dba9d5aae
zewdu erkyhun <zewdu444@gmail.com>
2023-10-12 21:40:38 +0300

    Add React chart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Stack } from "@mui/material";
-import ButtonComponent from "./components/ButtonComponent";
+// import ButtonComponent from "./components/ButtonComponent";
+import PrintComponent from "./components/PrintComponent";
 function App() {
   return (
-    <Stack
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <ButtonComponent />
+    // <Stack
+    //   sx={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     height: "100vh",
+    //     alignItems: "center",
+    //     justifyContent: "center",
+    //   }}
+    // >
+    //   <ButtonComponent />
+    // </Stack>
+    <Stack>
+      <PrintComponent />
     </Stack>
   );
 }
--- src/components/PrintComponent.js
+++ src/components/PrintComponent.js
@@ -1,27 +1,144 @@
-import { Box, Divider, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Stack,
+  Typography,
+  Container,
+  colors,
+} from "@mui/material";
 import React, { forwardRef } from "react";
-
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
 export const PrintComponent = forwardRef((props, ref) => {
+  const data = [
+    {
+      date_of_year: "2011",
+      burglary: 4000,
+    },
+    {
+      date_of_year: "2012",
+      burglary: 3000,
+    },
+    {
+      date_of_year: "2013",
+      burglary: 2000,
+    },
+    {
+      date_of_year: "2014",
+      burglary: 2780,
+    },
+    {
+      date_of_year: "2015",
+      burglary: 1890,
+    },
+    {
+      date_of_year: "2016",
+      burglary: 2390,
+    },
+    {
+      date_of_year: "2017",
+      burglary: 800,
+    },
+  ];
+
   return (
-    <div style={{ display: "none" }}>
+    <div style={{ display: "block" }}>
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
-          width: "100%",
+          width: "90%",
           height: "100%",
+          color: "text.primary",
+          p: 2,
+          mt: 2,
         }}
         ref={ref}
       >
         <Stack>
-          <Typography>Header</Typography>
-          <Divider />
+          <Typography
+            variant='h6'
+            sx={{
+              textAlign: "left",
+            }}
+          >
+            Crime
+          </Typography>
+          <Divider
+            sx={{
+              color: "blue",
+              bgcolor: "blue",
+              height: "2px",
+              marginLeft: "60px",
+              marginTop: "-15px",
+            }}
+          />
         </Stack>
         <Stack>
-          <Typography>Body</Typography>
-          <Divider />
+          <Container>
+            <Box
+              sx={{
+                bgcolor: "#f2f4f5",
+                height: "78vh",
+                borderRadius: "35px",
+                width: "103%",
+                mt: 4,
+              }}
+            >
+              <Typography
+                sx={{
+                  p: 2,
+                  backgroundColor: "#E8EEFB",
+                  borderRadius: "35px 35px 0 0",
+                  color: "blue",
+                  fontWeight: "bold",
+                }}
+                variant='body1'
+                component='article'
+              >
+                Burglary
+              </Typography>
+              <Stack
+                sx={{
+                  backgroundColor: "white",
+                  width: "90%",
+                  my: 3,
+                  mx: 6,
+                  borderRadius: "35px",
+                }}
+              >
+                <LineChart
+                  width={500}
+                  height={350}
+                  data={data}
+                  margin={{ top: 20, right: 30, bottom: 5 }}
+                >
+                  <CartesianGrid y={1000} stroke='lightgray' />
+                  <XAxis dataKey='date_of_year' />
+                  <YAxis />
+                  <Tooltip />
+                  <Line
+                    type='linear'
+                    dataKey='burglary'
+                    stroke='blue'
+                    dot={false}
+                  />
+                </LineChart>
+              </Stack>
+            </Box>
+          </Container>
         </Stack>
-        <Stack>Footer</Stack>
+        <Stack>
+          <Divider />
+          Footer
+        </Stack>
       </Box>
     </div>
   );
